5a8944d122b1b7e5cc4dcf357afceee722ef70f1
mauricio <maurez89@yahoo.es>
2025-07-21 11:41:55 -0600

    push acr changes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,3 +3,13 @@ variable "tenant_id" {}
 variable "azure_rbac_enabled" {
   type = string
 }
+variable "name_proxy_db_username" {
+  default = "user"
+}
+variable "location" {
+  default = "mexicocentral"
+}
+variable  "resource_group_name" {
+  default = "rg-dev-eastus"
+}
+
