36b68f4f32041466adfe06e8a33aeea468576f64
mauricio <maurez89@yahoo.es>
2025-07-30 15:52:15 -0600

    docx postgres rpo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,11 +11,11 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   kubernetes_version = var.kubernetes_version
   # enable RBAC
-  role_based_access_control_enabled = true
+  role_based_access_control_enabled = var.role_based_access_control_enabled
   
   azure_active_directory_role_based_access_control {
     tenant_id              = var.tenant_id
-    admin_group_object_ids = [var.admin_group_object_id]
+    admin_group_object_ids = var.admin_group_object_id
     azure_rbac_enabled = false
   }
 
